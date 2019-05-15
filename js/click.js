d8d6261257067586444c1a536e47325a1e246039
Antti Kuisma <anttiolavikuisma@gmail.com>
2019-05-15 17:50:54 +0300

    Updates

--- js/click.js
+++ js/click.js
@@ -1,6 +1,8 @@
 // work.html show/hide project description texts
 $(document).ready(function () {
-    $(".btn-light").hover(function () {
+    $(".btn-light").click(function () {
         $('.desc-text', this).stop().toggle(500);
+        $('.arrow-down', this).toggle();
+        $('.arrow-up', this).toggle();
     });
 });
